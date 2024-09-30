7616ce22b0ff872a5c9ac745a5fdefbd6b337cc3
BrivaHamisi <brivahamisi@gmail.com>
2024-09-30 15:22:25 +0300

    Generics API Vied Added

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.product_list),
+    path('products/', views.ProductCreateAPIView.as_view()),
+    path('products/<int:pk>/', views.ProductDetailAPIView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-
+from rest_framework import generics
 from products.models import Product
 from products.serializers import ProductSerializer
 
@@ -14,7 +14,23 @@ def product_list(request, *args, **kwargs):
     # if instance:
     #     data = ProductSerializer(instance).data
     serializer = ProductSerializer(data=request.data)
-    if serializer.is_valid():
+    if serializer.is_valid(raise_exception=True):
         instance = serializer.save()
         print(instance)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class ProductCreateAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        print(serializer)
+        serializer.save()
+        # serializer.save(owner=self.request.user)
+
+
+
+class ProductDetailAPIView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # lookup_field = 'pk'
